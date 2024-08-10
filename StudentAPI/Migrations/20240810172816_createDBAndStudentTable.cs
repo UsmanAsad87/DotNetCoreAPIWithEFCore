e5f344475ac927111333e8d1994dc2c5aee57c2f
usmanasad87 <usmanasad0324@gmail.com>
2024-08-10 22:43:34 +0500

    Add project files.

---
+++ StudentAPI/Migrations/20240810172816_createDBAndStudentTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentAPI.Migrations
+{
+    public partial class createDBAndStudentTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
